f1461ed7e56cd963eaf02aa868e1935cd5b579c2
Gehad Ahmed <gehad2363@gmail.com>
2026-05-19 22:34:32 +0300

    Connect angular services to my-json-server API

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  baseUrl: "http://localhost:3000"
+  // baseUrl: "http://localhost:3000"
+  baseUrl: "https://my-json-server.typicode.com/gehadahmed23/Healthcare-Appointment-System"
 };
